0bea88a8eff319aeb1bff46a083ab160daa95bc1
pritam dutta <webdevpritam7498@gmail.com>
2025-03-01 20:18:33 +0600

    set the global and custom component styling with css

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import style from './about.module.css';
 
 export default function About() {
   return (
     <div className='text-center'>
-        <h2>About Page</h2>
+        <h2 className="test">About Page</h2>
+        <p>This is a test page.</p>
+        <p className={style['custom-css']}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa magni accusamus minus nemo possimus, eveniet mollitia neque aspernatur delectus debitis. </p>
     </div>
   )
 }
