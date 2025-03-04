3768d347e4da287bd8de7d4bdb6f1a73679e4a4a
pritam dutta <webdevpritam7498@gmail.com>
2025-03-04 21:05:19 +0600

    patch/updated singel data

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -1,6 +1,7 @@
 import dbConnect from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
 
+//get the singel data from database
 export async function GET(req, {params}) {
   const p = await params;
   const singelData = await dbConnect("practice_data").findOne({ _id: new ObjectId(p.id)});
@@ -9,15 +10,20 @@ export async function GET(req, {params}) {
 }
 
 
-export async function DELETE(req, params) {
+export async function DELETE(req, {params}) {
   const p = await params;
   console.log(p);
 
   return Response.json({ params: p });
 }
-export async function PATCH(req, params) {
-  const p = await params;
-  console.log(p);
 
-  return Response.json({ params: p });
+//update the singel data from database
+export async function PATCH(req, {params}) {
+
+  const postedData = await req.json()
+  const p = await params;
+  const filter  =  ({ _id: new ObjectId(p.id)})
+  const updatedData = await dbConnect("practice_data").updateOne(filter, {$set: {...postedData} }, {upsert: true});
+
+  return Response.json(updatedData);
 }
